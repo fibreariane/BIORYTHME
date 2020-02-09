235a2ce552493d48d5ace4b2fdddae2de9fca4df
fibre <fibredarianeapp@gmail.com>
2020-02-09 15:21:14 +0100

    AJOUT nom version et Id Biorythme
    Ajout de l'affichage de la version et de l'identifiant du biorythme en cours dans la fenêtre paramètre

--- app/src/main/java/com/fibredariane/horoscope/chinois/biorythmeETRES/Activities/ParameterFragment.java
+++ app/src/main/java/com/fibredariane/horoscope/chinois/biorythmeETRES/Activities/ParameterFragment.java
@@ -10,12 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.fibredariane.horoscope.chinois.biorythmeETRES.BuildConfig;
 import com.fibredariane.horoscope.chinois.biorythmeETRES.R;
 import com.fibredariane.horoscope.chinois.biorythmeETRES.Test.TestBinomeActivity;
 import com.fibredariane.horoscope.chinois.biorythmeETRES.Test.TestMain;
+import com.fibredariane.horoscope.chinois.biorythmeETRES.Utils.Preferences;
 
+import java.util.Date;
+import java.util.TimeZone;
 
 public class ParameterFragment extends Fragment implements View.OnClickListener {
 
@@ -68,11 +72,16 @@ public class ParameterFragment extends Fragment implements View.OnClickListener 
             mLinearLayoutGlossaire.setOnClickListener(this);
             mLinearLayoutEvaluation = (LinearLayout) rootView.findViewById(R.id.linear_layout_evaluation);
             mLinearLayoutEvaluation.setOnClickListener(this);
-            mButtonTest= (Button) rootView.findViewById(R.id.btn_test);
+            mButtonTest = (Button) rootView.findViewById(R.id.btn_test);
             if (BuildConfig.DEBUG) {
                 mButtonTest.setVisibility(View.VISIBLE);
                 mButtonTest.setOnClickListener(this);
             }
+            TextView textViewVersion = rootView.findViewById(R.id.version_name);
+            textViewVersion.setText(BuildConfig.VERSION_NAME);
+
+            TextView textViewIdBio = rootView.findViewById(R.id.biorythmme_id);
+            textViewIdBio.setText(getIdBiorythme());
 
             mFragmentManager = getFragmentManager();
             mDialogueOuiNon = new DialogOuiNonFragment();
@@ -86,6 +95,109 @@ public class ParameterFragment extends Fragment implements View.OnClickListener 
         return rootView;
     }
 
+    private String getIdBiorythme() {
+        String id = getString(R.string.pas_renseigne);
+
+        String date_biorythme = new Preferences().getStringDatePref();
+        if (date_biorythme != "") {
+            String[] date = date_biorythme.split("\\.");
+            id = Integer.parseInt(date[2])
+                    + getLetterNumber(Integer.parseInt(date[1]))
+                    + String.valueOf(Integer.parseInt(date[0])).substring(2, 4)
+                    + getLetterNumber(Integer.parseInt(date[3]))
+                    + getLetterMinute(Integer.parseInt(date[4]))
+                    + TimeZone.getDefault().getDisplayName();
+        }
+        return id;
+    }
+
+    private String getLetterMinute(int minute) {
+        if (minute < 30) {
+            return "Y";
+        } else {
+            return "Z";
+        }
+    }
+
+    private String getLetterNumber(int number) {
+        String letter = "";
+        switch (number) {
+            case 0:
+            case 24:
+                letter = "A";
+                break;
+            case 1:
+                letter = "B";
+                break;
+            case 2:
+                letter = "C";
+                break;
+            case 3:
+                letter = "D";
+                break;
+            case 4:
+                letter = "E";
+                break;
+            case 5:
+                letter = "F";
+                break;
+            case 6:
+                letter = "G";
+                break;
+            case 7:
+                letter = "H";
+                break;
+            case 8:
+                letter = "I";
+                break;
+            case 9:
+                letter = "J";
+                break;
+            case 10:
+                letter = "K";
+                break;
+            case 11:
+                letter = "L";
+                break;
+            case 12:
+                letter = "M";
+                break;
+            case 13:
+                letter = "N";
+                break;
+            case 14:
+                letter = "O";
+                break;
+            case 15:
+                letter = "P";
+                break;
+            case 16:
+                letter = "Q";
+                break;
+            case 17:
+                letter = "R";
+                break;
+            case 18:
+                letter = "S";
+                break;
+            case 19:
+                letter = "T";
+                break;
+            case 20:
+                letter = "U";
+                break;
+            case 21:
+                letter = "V";
+                break;
+            case 22:
+                letter = "W";
+                break;
+            case 23:
+                letter = "X";
+        }
+        return letter;
+    }
+
     @Override
     public void onClick(View v) {
         Intent intent;
@@ -93,16 +205,16 @@ public class ParameterFragment extends Fragment implements View.OnClickListener 
             case R.id.linear_layout_reset:
                 mDialogueOuiNon.show(mFragmentManager, "Sample Fragment");
                 break;
-            case R.id.linear_layout_switch :
-                 intent = new Intent(mContext, SwitchBiorythmeActivity.class);
+            case R.id.linear_layout_switch:
+                intent = new Intent(mContext, SwitchBiorythmeActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.linear_layout_langue :
-            case R.id.linear_layout_achat :
-            case R.id.linear_layout_glossaire :
-            case R.id.linear_layout_evaluation :
+            case R.id.linear_layout_langue:
+            case R.id.linear_layout_achat:
+            case R.id.linear_layout_glossaire:
+            case R.id.linear_layout_evaluation:
                 mDialogueInformation.show(mFragmentManager, "Sample Fragment");
-               break;
+                break;
             case R.id.btn_test:
                 intent = new Intent(mContext, TestMain.class);
                 startActivity(intent);
